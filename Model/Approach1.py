a8d3075fb0ef59e83e8c56ec7f21f1f2da75c458
Konrad <krukarkonrad@gmail.com>
2020-05-01 00:49:50 +0200

    video + model train

--- Model/Approach1.py
+++ Model/Approach1.py
@@ -1,116 +1,101 @@
-#Const
-width, height, depth = 48, 48, 3
-numOfClasses = 7
+#CONST
+imageSize, depth = 48, 3
+numberOfClasses = 7
+filePath = 'data.csv'
 
-#Load data
-import pandas as pd
-dataFile = pd.read_csv('data.csv')
-print(dataFile.shape)
-trainSamples = dataFile[dataFile['Usage'] == "Training"]
-testSamples = dataFile[dataFile["Usage"] == "PrivateTest"]
-validationSamples = dataFile[dataFile["Usage"] == "PublicTest"]
-
-#Load image
 import numpy as np
-pixelMatrix = np.array([int(string)/255 for string in trainSamples.iloc[0,1].split(' ')])
-pixelMatrix = pixelMatrix.reshape(width, height)
-
-#Show image
-import matplotlib.pyplot as plt
-plt.imshow(pixelMatrix, cmap='gray');
-
-#Prepare data for model
-def prepareData(dataSet):
-  #1D -> 2D
-  xTrain = []
-  for i in range(len(dataSet)):
-    pixelMatrix = np.array([int(string) for string in dataSet.iloc[i,1].split(' ')])
-    pixelMatrix = pixelMatrix.reshape(width, height)
-    xTrain.append(pixelMatrix)
-
-  x = np.array(xTrain)
-  yTrain = dataSet.iloc[:,0]
-
-  #Categorize categories to be compatible with softmax activation
-  from keras.utils import to_categorical
-  y = to_categorical(np.array(yTrain))
-  return x, y
-
-xTrain, yTrain = prepareData(trainSamples)
-print(xTrain.shape)
-print(yTrain.shape)
-
-xTest, yTest = prepareData(testSamples)
-print(xTest.shape)
-print(yTest.shape)
-
-xVal, yVal = prepareData(validationSamples)
-print(xVal.shape)
-print(yVal.shape)
-
-#Reshape to 4D, as VGG16 requires
-xTrainReshaped = np.expand_dims(xTrain, axis = 3)
-xTestReshaped = np.expand_dims(xTest, axis = 3)
-xValReshaped = np.expand_dims(xVal, axis = 3)
-
-#Repeat to get (:, :, :, 3)
-xTrainReshaped = np.repeat(xTrainReshaped, 3, axis = 3)
-xTestReshaped = np.repeat(xTestReshaped, 3, axis = 3)
-xValReshaped = np.repeat(xValReshaped, 3, axis = 3)
-
-print(xTrainReshaped.shape)
-print(yTrain.shape)
-print(xTestReshaped.shape)
-print(yTest.shape)
-print(xValReshaped.shape)
-print(yVal.shape)
-
-#Import VGG16 without top layers
+import pandas as pd
 from keras.applications.vgg16 import VGG16
-baseModel = VGG16(include_top= False, weights='imagenet', input_shape = (width,height,depth))
-
-baseModel.summary()
-
-print(baseModel.predict([[xTrainReshaped[0]]]).shape)
-
-#Create own model
+from keras.layers import Dense, Dropout
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout
+from keras.optimizers import Adamax
 
-myModel = Sequential()
+TRAIN_END = 28708
+TEST_START = TRAIN_END + 1
 
-myModel.add(baseModel)
-myModel.add(Flatten())
-myModel.add(Dense(512, input_shape=(512,), activation='relu'))
-myModel.add(Dense(256, input_shape=(256,),activation='relu'))
-myModel.add(Dropout(0.5))
-myModel.add(Dense(128, input_shape=(256,)))
-myModel.add(Dense(7, activation='softmax'))
 
-myModel.summary()
+def split_for_test(list):
+    train = list[0:TRAIN_END]
+    test = list[TEST_START:]
+    return train, test
 
-myModel.compile(loss='categorical_crossentropy',
-                optimizer="sgd",
-                metrics=['accuracy'])
+def processPixels(pixels, img_size=imageSize):
 
-for layer in myModel.layers[:1]:
-   layer.trainable=False
-for layer in myModel.layers[1:]:
-   layer.trainable=True
+    pixels_list = [item[0] for item in pixels.values.tolist()]
 
-for layer in myModel.layers:
-  print(layer.name)
-  print(layer.trainable)
+    score_array = []
+    for index, item in enumerate(pixels_list):
+        data = np.zeros((imageSize, imageSize), dtype=np.uint8)
+        pixel_data = item.split()
 
-#Fit params
-noEpochs = 50
-batchSize = 100
-history = myModel.fit(xTrainReshaped , yTrain,
-                      epochs=noEpochs, batch_size=batchSize,
-                      validation_data=(xValReshaped, yVal))
+        for i in range(0, imageSize):
+            index = i * imageSize
+            data[i] = pixel_data[index:index + imageSize]
 
-myModel.save("test.h5")
+        score_array.append(np.array(data))
 
-totest = 2
-print(myModel.predict([[xTrainReshaped[totest]]])*10)
-print(yTest[totest])
+    score_array = np.array(score_array)
+    score_array = score_array.astype('float32') / 255.0
+    return score_array
+
+
+def makeVGG16input(array_input):
+    array_input = np.expand_dims(array_input, axis=3)
+    array_input = np.repeat(array_input, 3, axis=3)
+    return array_input
+
+raw_data = pd.read_csv(filePath)
+
+# convert to one hot vectors
+from keras.utils import to_categorical
+
+emotion_array = to_categorical(np.array(raw_data[['emotion']]))
+
+# convert to a 48x48 float matrix
+pixel_array = processPixels(raw_data[['pixels']])
+
+# split for test/train
+y_train, y_test = split_for_test(emotion_array)
+x_train_matrix, x_test_matrix = split_for_test(pixel_array)
+
+x_train_input = makeVGG16input(x_train_matrix)
+x_test_input = makeVGG16input(x_test_matrix)
+
+print("Data loading - DONE!")
+print("x_train_input ", x_train_input.shape)
+
+# vgg 16. include_top=False so the output is the 512 and use the learned weights
+vgg16 = VGG16(include_top=False, input_shape=(48, 48, 3), pooling='avg', weights='imagenet')
+
+for layer in vgg16.layers:
+    layer.trainable = False
+
+# build and train model
+fialLayer = Sequential()
+fialLayer.add(vgg16)
+fialLayer.add(Dense(256, input_shape=(512,), activation='relu'))
+fialLayer.add(Dense(256, input_shape=(256,), activation='relu'))
+fialLayer.add(Dropout(0.5))
+fialLayer.add(Dense(128, input_shape=(256,)))
+fialLayer.add(Dense(numberOfClasses, activation='softmax'))
+fialLayer.summary()
+adamax = Adamax()
+
+fialLayer.compile(loss='categorical_crossentropy',
+                  optimizer="sgd",
+                  metrics=['accuracy'])
+
+# train
+history = fialLayer.fit(
+    x_train_input, y_train,
+    validation_data=(x_test_input, y_test),
+    epochs=100,
+    batch_size=50)
+
+# Evaluate
+score = fialLayer.evaluate(x_test_input,
+                           y_test, batch_size=50)
+
+print("After top_layer_model training (test set): {}".format(score))
+
+fialLayer.save('model.h5')
